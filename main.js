0113b516e592b7e0f857ceaee28b8174e9e8b7aa
Gjarred Baker <gjarred23@gmail.com>
2024-01-04 16:28:20 -0600

    fixed twosum

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ var twoSum = function(nums, target){
   for(i=0;i<nums.length;i++){
       for(j=1;j<nums.length;j++){
           //console.log(nums[i],nums[j])
-          if(nums[i] + nums[j] == target){
+          if(nums[i] + nums[j] == target && i !== j){
               console.log([i,j])
           }
       }
