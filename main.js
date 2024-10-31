5da50facf71cf00918764d2710c2da12640e4fa2
Gjarred Baker <gjarred23@gmail.com>
2024-10-31 13:21:39 -0500

    max Area

--- main.js
+++ main.js
@@ -72,3 +72,75 @@ var twoSum = function(nums, target){
 };
 
 twoSum([2,7,11,15], 9)
+
+
+//11. Container With Most Water
+//Medium
+//
+//Topics
+//Companies
+//
+//Hint
+//You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+//
+//Find two lines that together with the x-axis form a container, such that the container contains the most water.
+//
+//Return the maximum amount of water a container can store.
+//
+//Notice that you may not slant the container.
+//
+// 
+//
+//Example 1:
+//
+//
+//Input: height = [1,8,6,2,5,4,8,3,7]
+//Output: 49
+//Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+//Example 2:
+//
+//Input: height = [1,1]
+//Output: 1
+// 
+//
+//Constraints:
+//
+//n == height.length
+//2 <= n <= 105
+//0 <= height[i] <= 104
+
+//nested forloop
+  //first loop -loops through each height  
+  //second loop checks against the other heights
+  //with the two numbers find the lowest num of the two 
+  // Area = height * length = multiply the lowest num and the amount of indices between the first and second num
+
+  //now check with one is the highest
+  //create variable for highArea = [];
+  //if current Area > Area then Area  = current Area
+
+ 
+
+  var maxArea = function(height){
+
+    let highestArea = 0;
+    
+   for(i=0;i<height.length;i++){
+    for(j=0;j<height.length;j++){
+        cl(height[i],height[j])
+       lowestH =  Math.min(height[i],height[j])
+       length = Math.max([i],[j]) - Math.min([i],[j])
+       area = lowestH * length
+       cl(lowestH)
+       cl(length)
+       cl(area)
+       if(area > highestArea){
+        highestArea = area
+       }else highestArea + 0
+    }
+   }
+   return highestArea
+
+}
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7]))
