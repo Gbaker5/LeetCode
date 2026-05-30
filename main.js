62e4543394a276a31bf6de4eb31f6fa0c832ac61
Gjarred Baker <gjarred23@gmail.com>
2026-05-30 13:12:50 -0500

    Find First and Last Position of Element in Sorted Array

--- main.js
+++ main.js
@@ -940,3 +940,57 @@ reverse(-123)
 //Reading stops at the first non-digit character 'w'.
 
  
+
+//34. Find First and Last Position of Element in Sorted Array
+//Solved
+//Medium
+//
+//Topics
+//premium lock icon
+//Companies
+//Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+//
+//If target is not found in the array, return [-1, -1].
+//
+//You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+//Example 1:
+//
+//Input: nums = [5,7,7,8,8,10], target = 8
+//Output: [3,4]
+//Example 2:
+//
+//Input: nums = [5,7,7,8,8,10], target = 6
+//Output: [-1,-1]
+//Example 3:
+//
+//Input: nums = [], target = 0
+//Output: [-1,-1]
+// 
+//
+//Constraints:
+//
+//0 <= nums.length <= 105
+//-109 <= nums[i] <= 109
+//nums is a non-decreasing array.
+//-109 <= target <= 109
+//
+
+var searchRange = function(nums, target) {
+    let first = nums.indexOf(target)
+    let last = nums.lastIndexOf(target)
+
+    //console.log(first)
+    //console.log(last)
+    if(first > -1 && nums.length == 1){
+      return [0,0]
+    }else if(first > -1 && last){
+      return [first,last]
+    }else if(first > -1){
+      return [first,first]
+    }else return [-1,-1]
+};
+
+searchRange([5,7,7,8,8,10])
